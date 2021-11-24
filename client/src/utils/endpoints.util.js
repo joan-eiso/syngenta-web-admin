4b67a9e1803f3c8d84e81467d9c1668676ac187f
--global <joan.caliz@eiso.com.co>
2021-11-24 11:30:26 -0500

    New deployment test

--- client/src/utils/endpoints.util.js
+++ client/src/utils/endpoints.util.js
@@ -1,2 +1,2 @@
-export const authEndpoint = "http://localhost:80/api/auth";
-export const sessionEndpoint = "http://localhost:80/api/session";
+export const authEndpoint = "http://localhost:4000/api/auth";
+export const sessionEndpoint = "http://localhost:4000/api/session";
