a1bdc8a283f2feb63fc583a4d76201193dd89c0f
--global <joan.caliz@eiso.com.co>
2021-11-24 12:01:11 -0500

    cors-anywhere deploy

--- client/src/utils/endpoints.util.js
+++ client/src/utils/endpoints.util.js
@@ -1,2 +1,5 @@
-export const authEndpoint = "http://localhost:80/api/auth";
-export const sessionEndpoint = "http://localhost:80/api/session";
+export const authEndpoint = "https://cors-anywhere.herokuapp.com/https://6vc5bahvj9.execute-api.us-east-2.amazonaws.com/stage_syngenta_pdn/unregistred-user";
+export const sessionEndpoint = "https://cors-anywhere.herokuapp.com/https://xtdb9nxmja.execute-api.us-east-2.amazonaws.com/stage_syngenta_pdn_logged/syngenta-logged-pdn";
+
+// export const authEndpoint = "http://localhost:8080/api/auth";
+// export const sessionEndpoint = "http://localhost:8080/api/session";
