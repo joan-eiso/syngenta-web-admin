df884a6402255114573c668291f79819c22fb566
--global <joan.caliz@eiso.com.co>
2021-11-24 12:11:10 -0500

    Fix wrong host endpoint

--- client/src/utils/endpoints.util.js
+++ client/src/utils/endpoints.util.js
@@ -1,2 +1,2 @@
-export const authEndpoint = "http://localhost:4000/api/auth";
-export const sessionEndpoint = "http://localhost:4000/api/session";
+export const authEndpoint = "http://18.219.255.14:4000/api/auth";
+export const sessionEndpoint = "http://18.219.255.14:4000/api/session";
